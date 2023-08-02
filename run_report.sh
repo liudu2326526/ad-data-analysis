57dd985ddb3d194b8d7ca38640532e30eb536073
liudu <liudu2326526>
2023-08-02 12:59:50 +0800

    add

--- run_report.sh
+++ run_report.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# 获取当前工作目录
+current_dir=$(pwd)
+
+# 获取脚本的相对路径
+script_relative_path="$0"
+
+# 将脚本相对路径转换为绝对路径
+script_absolute_path="${current_dir}/${script_relative_path}"
+
+# 提取脚本所在的目录部分
+script_directory=$(dirname "$script_absolute_path")
+
 # shellcheck disable=SC2164
-cd script/src
+cd $script_directory/script/src
 
 # 获取传入的第一个参数
 if [ $# -eq 0 ]; then
