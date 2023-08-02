afc4317ca9dca0e6d031f15563b178e6cf67b4d7
liudu <liudu2326526>
2023-08-02 14:40:35 +0800

    add

--- run_report.sh
+++ run_report.sh
@@ -3,17 +3,11 @@
 #0 1 * * * /bin/bash /home/ec2-user/ad-data-analysis/run_report.sh 3
 #0 * * * * /bin/bash /home/ec2-user/ad-data-analysis/run_report.sh
 
-# 获取当前工作目录
-current_dir=$(pwd)
-
 # 获取脚本的相对路径
 script_relative_path="$0"
 
-# 将脚本相对路径转换为绝对路径
-script_absolute_path="${current_dir}/${script_relative_path}"
-
 # 提取脚本所在的目录部分
-script_directory=$(dirname "$script_absolute_path")
+script_directory=$(dirname "$script_relative_path")
 
 # shellcheck disable=SC2164
 cd $script_directory/script/src
